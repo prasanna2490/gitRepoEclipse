204c5ed13a181be7c216f79b9708674840d88251
prasanna2490 <shindeprasanna24@gmail.com>
2021-04-04 10:45:15 +0530

    firstCommit

---
+++ src/main/java/gitRepoDemo/SImpleClass.java
@@ -0,0 +1,11 @@
+package gitRepoDemo;
+
+public class SImpleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("THis is simple java class");
+	}
+
+}
