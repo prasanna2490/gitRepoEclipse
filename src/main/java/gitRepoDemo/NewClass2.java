c878d3211a72e78a314db27ab89520f2cee9a8cb
prasanna2490 <shindeprasanna24@gmail.com>
2021-04-04 11:19:09 +0530

    commit to new branch BranchNewPras

---
+++ src/main/java/gitRepoDemo/NewClass2.java
@@ -0,0 +1,11 @@
+package gitRepoDemo;
+
+public class NewClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("THis is my new claas2 in local repo");
+	}
+
+}
